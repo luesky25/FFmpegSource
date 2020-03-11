d7923836f1a288c28c1eae5855303531d3bda20c
danny <luesky2020@gmail.com>
2020-03-11 16:57:34 +0800

    1

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,32 @@
+
+
+
 #include <jni.h>
 #include <string>
 
+//#include "libavcodec/avcodec.h"
+//
+//
+//extern "C"
+//JNIEXPORT jstring JNICALL
+//Java_coding_yu_helloffmpeg_MainActivity_string2FromJNI(JNIEnv *env, jobject instance) {
+//
+//    char info[10000] = {0};
+//    sprintf(info, "%s\n", avcodec_configuration());
+//
+//    return env->NewStringUTF(info);
+//}
+
+
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_google_zxing_client_ffmpegsource_MainActivity_stringFromJNI(
-        JNIEnv *env,
+        JNIEnv* env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
+
+
+
+
